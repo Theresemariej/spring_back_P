44bb8a9ca200775742412630d5bde70dddd7da49
unknown <theresemariejaze@gmail.com>
2024-04-24 14:11:22 +0200

    verif

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO VERTI(libelle, oui, non) VALUES
     ('Y-a-t-il des drapeaux rouges ?','oui', 'non'),
     ('Y-a-t-il des vertiges maintenant ?','oui', 'non'),
     ('Y-a-t-il un ou plusieurs Hints ?','oui', 'non'),
-    ('Y-a-t-il des un nystagamus spontanné ?','oui', 'non'),
-    ('Urgence?','oui', 'non');
+    ('S`agit-il d`un Nystagmus spontané ?','oui', 'non'),
+    ('Le patient doit aller aux urgences','oui', 'non');
 
